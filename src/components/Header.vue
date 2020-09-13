e431ec6837ad42eac8434a1d11c80097206176c1
ttack-hub0706 <tkm_fjhr0706@icloud.com>
2020-09-14 00:00:43 +0900

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="header">
+    <nav>
+      <ul>
+        <li>
+          <a href="#">ホーム</a>
+        </li>
+        <li>
+          <a href="#">詳細</a>
+        </li>
+        <li>
+          <a href="#">お問い合わせ</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+}
+li {
+  margin-right: 50px;
+}
+</style>
